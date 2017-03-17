e879a2d3b8bdb46c235decbf3cf18133c359fd3d
zanoooda <zanooda@ya.ru>
2017-03-17 14:30:53 +0200

    Add phaser.js css and emty game template

---
+++ js/game.js
@@ -0,0 +1,18 @@
+
+var game = new Phaser.Game("100%", "100%", Phaser.CANVAS, '', {
+    preload: preload,
+    create: create,
+    update: update
+});
+
+function preload() {
+    
+}
+
+function create() {
+    
+}
+
+function update() {
+    
+}
