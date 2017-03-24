c0276ad96369a4b55e0c057422c97d8f26024fab
zanoooda <zanooda@ya.ru>
2017-03-24 17:13:10 +0300

    Bee animation

--- js/game.js
+++ js/game.js
@@ -8,23 +8,24 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game-container', {
 // Game objects
 var cursors;
 
-//var obstacles;
+// Groups
 var platforms;
 var clouds;
-var zokee;
 var honey;
+var zokee;
 var enemies;
 
+// Player
 var bada;
 
 // Preload media
 function preload() {
     game.load.spritesheet('bada', 'assets/images/bada.png', 48, 48);
+    game.load.spritesheet('bee', 'assets/images/bee.png', 32, 32);
     game.load.image('ground', 'assets/images/ground.png');
     game.load.image('zok', 'assets/images/zok.png');
     game.load.image('cloud', 'assets/images/cloud.png');
     game.load.image('honey', 'assets/images/honey.png');
-    game.load.image('bee', 'assets/images/bee.png');
 }
 
 // Create game
@@ -38,14 +39,6 @@ function create() {
     // Inputs from keyboard
     cursors = game.input.keyboard.createCursorKeys();
 
-    // Platforms
-    platforms = createGroup();
-
-    createPlatform(0, (game.world.height / 4) * 3, 'ground');
-    createPlatform(500, (game.world.height / 4) * 3, 'ground');
-    createPlatform(1000, (game.world.height / 4) * 3, 'ground');
-    createPlatform(1500, (game.world.height / 4) * 3, 'ground');
-
     // Clouds
     clouds = createGroup();
 
@@ -55,10 +48,14 @@ function create() {
     createCloud(1500, 100, 'cloud');
     createCloud(1900, 100, 'cloud');
 
-    // Zokee
-    zokee = createGroup();
-    
-    createZok(800, 200, 'zok');
+    // Platforms
+    platforms = createGroup();
+
+    createPlatform(0, 150, 'ground');
+    createPlatform(0, (game.world.height / 4) * 3, 'ground');
+    createPlatform(500, (game.world.height / 4) * 3, 'ground');
+    createPlatform(1000, (game.world.height / 4) * 3, 'ground');
+    createPlatform(1500, (game.world.height / 4) * 3, 'ground');
 
     // Honey
     honey = createGroup();
@@ -71,10 +68,16 @@ function create() {
     createHoney(1600, 200, 'honey');
     createHoney(1800, 200, 'honey');
 
+    // Zokee
+    zokee = createGroup();
+    
+    createZok(700, 200, 'zok');
+
     // Enemies
     enemies = createGroup();
 
     createEnemy(250, 440, 'bee');
+    createEnemy(600, 440, 'bee');
 
     //Bada
     bada = createPlayer(50, 150, 'bada');
@@ -82,68 +85,46 @@ function create() {
 
 // Update game
 function update() {
-    bada.sit = false;
-
     game.physics.arcade.collide(honey, platforms);
 
-    //game.physics.arcade.collide(enemies, platforms);
-
     game.physics.arcade.collide(zokee, platforms);
-    //game.physics.arcade.collide(zokee, zokee);
 
     game.physics.arcade.collide(bada, platforms);
-    game.physics.arcade.collide(bada, clouds, sit);
+    bada.sit = game.physics.arcade.collide(bada, clouds, sit);
     game.physics.arcade.collide(bada, zokee);
     game.physics.arcade.collide(bada, honey, collect);
     game.physics.arcade.collide(bada, enemies, recoil);
 
-    if (cursors.left.isDown && !bada.recoil) {
-        bada.animations.play('left');
-        bada.body.velocity.x = -150;
-    }
-    else if (cursors.right.isDown  && !bada.recoil) {
-        bada.animations.play('right');
-        bada.body.velocity.x = 150;
-    }
-    else {
-        bada.body.velocity.x = 0;
-        if(bada.recoil) {
-            bada.body.velocity.x = -50;
-            bada.animations.play('recoil');
+    if(bada.recoil) {
+        bada.body.velocity.x = -50;
+        bada.animations.play('recoil');
+    } else {
+        if (cursors.left.isDown) {
+            bada.animations.play('left');
+            bada.body.velocity.x = -150;
+        } else if (cursors.right.isDown) {
+            bada.animations.play('right');
+            bada.body.velocity.x = 150;
+        } else {
+            bada.body.velocity.x = 0;
+            if(!bada.sit) {
+                bada.animations.play('stand');
+            }
         }
-        if(!bada.sit && !bada.recoil) {
-            bada.animations.play('stand');
+
+        if (cursors.up.isDown && bada.body.wasTouching.down) {
+            bada.body.velocity.y = -200;
+        } else if (cursors.down.isDown) {
+            bada.animations.play('down');
+            bada.body.velocity.y = 150;
         }
     }
-
-    if (cursors.up.isDown)
-    {
-        bada.body.velocity.y = -200;
-    }
-    else if (cursors.down.isDown && !bada.recoil)
-    {
-        bada.animations.play('down');
-        bada.body.velocity.y = 150;
-    }
-}
-
-// TODO: learn what does it mean 'game.physics.arcade.enable(sprite)'
-function createEnemy(width, height, key) {
-    var bee = enemies.create(width, height, key);
-    bee.anchor.setTo(0.5, 1);
-    bee.body.immovable = true;
-
-    bee.tween = game.add.tween(bee).to({x: width, y: height - 100}, 2000, null, true, 0, -1, true);    
-    bee.tween.onComplete.add(function(){
-        beeTween.to({x: width, y: height}, 2000, null, true, 0, -1, true);   
-    });    
-
-    return bee;
 }
 
 // Creation functions
 function createGroup() {
-    // Is group.physicsBodyType = Phaser.Physics.ARCADE equal to game.physics.arcade.enable(sprite) ?
+    // TODO: learn what does it mean 'game.physics.arcade.enable(sprite)'
+    // Is 'group.physicsBodyType = Phaser.Physics.ARCADE' equal to 'game.physics.arcade.enable(sprite)' for each sprite?
 
     var group = game.add.group();
     group.enableBody = true;
@@ -190,6 +171,24 @@ function createCloud(width, height, key) {
     return cloud;
 }
 
+function createEnemy(width, height, key) {
+    // TODO: Make the enemies move asyncroniusly
+
+    var enemy = enemies.create(width, height, key);
+    enemy.anchor.setTo(0.5, 1);
+    enemy.body.immovable = true;
+
+    enemy.animations.add('zzz', [0, 1], 30, true);
+    enemy.animations.play('zzz');
+
+    enemy.tween = game.add.tween(enemy).to({x: width, y: height - 100}, 2000, null, true, 0, -1, true);    
+    enemy.tween.onComplete.add(function(){
+        enemy.tween.to({x: width, y: height}, 2000, null, true, 0, -1, true);   
+    });    
+
+    return enemy;
+}
+
 function createPlayer(width, height, key) {
     player = game.add.sprite(width, height, key);
 
@@ -198,12 +197,13 @@ function createPlayer(width, height, key) {
     
     player.anchor.setTo(0.5, 1);
     player.body.gravity.y = 300;
+
     player.animations.add('stand', [4], 20, true);
     player.animations.add('right', [0, 1, 2, 3], 10, true);
     player.animations.add('left', [5, 6, 7, 8], 10, true);
-    player.animations.add('down', [9], 20, true);
-    player.animations.add('sit', [10], 20, true);
-    player.animations.add('recoil', [9], 20, true);
+    player.animations.add('down', [9], 0, true);
+    player.animations.add('sit', [10], 0, true);
+    player.animations.add('recoil', [9], 0, true);
 
     return player;
 }
@@ -219,13 +219,17 @@ function collect(bada, item) {
 
 function sit() {
     if(!cursors.right.isDown && !cursors.left.isDown){
-        bada.sit = true;
         bada.animations.play('sit');
     }
 }
 
-function recoil(bada, item) {
-    // TODO: if recoil from down side of enemy it must stop (item.tween.pause(), item.tween.resume())
+function recoil(bada, enemy) {
+    // ISSUE: When player touch enemy fron up twise. First setTimeout make 'bada.recoil = false' when second recoil still is...
+    if(bada.body.wasTouching.up) {
+        enemy.tween.pause();
+    }
+
+    // TODO: if recoil from down side of enemy it must stop (enemy.tween.pause(), enemy.tween.resume())
     //       create logic to resume tween
     //
     // TODO: if from right side of enemy it must recoil to another side
@@ -238,5 +242,7 @@ function recoil(bada, item) {
     setTimeout(function() {
         //bada.body.velocity.x = 0;
         bada.recoil = false;
+        // Better to resume tween directly whan player stop to touching enemy from up
+        enemy.tween.resume();
     }, 1500);
 }
